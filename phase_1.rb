c3dd6a203074c6ae162335fb06f79ccd796bab4b
Lindsay Nauman <lindsay.nauman@gmail.com>
2013-08-28 15:31:20 -0400

    phase 2 woooo

--- phase_1.rb
+++ phase_1.rb
@@ -1,28 +1,57 @@
+def identify_winner(hash)
+  new_hash = hash.sort_by {|team, score| score}
+  winner = new_hash.pop
+  winner[0]
+
+end
+
+
+team_list = {}
+
 puts "what was team 1's name?"
 print ">"
 one_name = gets.chomp
 print ">"
+team_list[one_name]
+
 puts "What was team 1's score?"
 print ">"
 one_score = gets.chomp
 print ">"
+team_list[one_name] = (one_score)
+
+
 puts "What was team 2's name?"
 print ">"
 two_name = gets.chomp
+team_list[two_name]
+
+
 puts "What was team 2's score?"
 print ">"
 two_score = gets.chomp
+team_list[two_name] = (two_score)
 
 
-# def check_score(one_score, two_score)
-  if one_score > two_score
-    puts "#{one_name} is the victor!"
-  elsif two_score > one_score
-    puts "#{two_name} is the victor!"
-  end
-# end
 
-# puts check_score
+
+
+print identify_winner(team_list)
+puts " is the victor!!!!"
+
+
+
+# how do i get them in a hash?????
+# compare value of each item in hash
+# print key of highest value
+# how do i compare numeric values in hashes 
+
+
+
+
+
+
+
 
 
 
@@ -37,4 +66,7 @@ two_score = gets.chomp
 # lohan = AddTeam.new('Lohan', 5)
 # puts lohan
 
+
+# identify the identify_winner
+# take 
 
